22b29f36f93e4c8ee3ec25969d80667f23c7ce94
emmanuelmarvin <emmanuelmarvin247@gmail.com>
2024-01-04 16:35:23 +0200

    adjustin pg

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const database = knex({
         // port: process.env.DB_PORT,
         // password: process.env.DB_PWD,
         // database: process.env.DB_NAME,
-        ssl: { rejectUnauthorized: false }
+        ssl: true
 
     },
 });
