61cc0ea8798623bc0c176a6227c66216562ded96
emmanuelmarvin <emmanuelmarvin247@gmail.com>
2024-01-04 16:48:17 +0200

    adjustinwwws

--- server.js
+++ server.js
@@ -14,11 +14,11 @@ const database = knex({
     version: "15",
     connection: {
         connectString: process.env.DB_CONNECT,
-        // host: process.env.DB_HOST,
-        // user: process.env.DB_USER,
-        // port: process.env.DB_PORT,
-        // password: process.env.DB_PWD,
-        // database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        port: process.env.DB_PORT,
+        password: process.env.DB_PWD,
+        database: process.env.DB_NAME,
         ssl: true
 
     },
