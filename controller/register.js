223d95f7eff874a0e4c3bacea7e815c652be4e97
emmanuelmarvin <emmanuelmarvin247@gmail.com>
2024-01-05 20:23:22 +0200

    supabase setup 2.10

--- controller/register.js
+++ controller/register.js
@@ -37,7 +37,7 @@ const handleRegister = async (req, res, supabase, bcrypt) => {
         } else {
             const { data, error } = await supabase
                 .from('users')
-                .select("*")
+                .select()
                 .eq('email', email)
             if (error) {
 
