886e7bc99edbc0fed4fb471de8c7cb08a90ef742
emmanuelmarvin <emmanuelmarvin247@gmail.com>
2024-01-06 15:43:59 +0200

    supabase setup 2.14.9

--- controller/signin.js
+++ controller/signin.js
@@ -4,7 +4,7 @@ const handleSign = async (req, res, supabase, bcrypt) => {
         return res.status(400).json('its a bad signin request over here');
     }
     const { data, error3 } = await supabase
-        .from('users')
+        .from('login')
         .select()
         .eq('email', email)
     if (error3) {
@@ -12,13 +12,22 @@ const handleSign = async (req, res, supabase, bcrypt) => {
         return res.status(400).json("failed to sign in")
 
     }
-    res.json(data)
-    // const isTrue = bcrypt.compareSync(password, data[0]['hash']);
-    // if (isTrue) {
-    //     res.json(data)
-    // } else {
-    //     res.status(400).json('Wrong credentials')
-    // }
+
+    const isTrue = bcrypt.compareSync(password, data[0]['hash']);
+    if (isTrue) {
+        const { data, error3 } = await supabase
+            .from('users')
+            .select()
+            .eq('email', email)
+        if (error3) {
+
+            return res.status(400).json("sign in error")
+
+        }
+        res.json(data[0])
+    } else {
+        res.status(400).json('Wrong credentials')
+    }
 
 
 }
